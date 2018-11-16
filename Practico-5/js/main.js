b1494b10b1981ca5ac469a7250703cb63580f019
Leiva Juan <Leiva Juan>
2018-11-16 12:46:04 -0300

    Avanze sobre los comentarios

--- Practico-5/js/main.js
+++ Practico-5/js/main.js
@@ -21,3 +21,30 @@ document.addEventListener("keydown", function(e){
             break;
     }
 });
+
+fetch('http://jsonplaceholder.typicode.com/users')
+  .then(responseUsers => responseUsers.json())
+  .then(function(responseUsers){
+    fetch('http://jsonplaceholder.typicode.com/photos')
+    .then(responsePhotos => responsePhotos.json())
+    .then(function(responsePhotos){
+        fetch('http://jsonplaceholder.typicode.com/posts?_start=10&_limit=10%27')
+        .then(response => response.json())
+        .then(function(response){
+            for (var i = 0; i < response.length; i++) {
+            document.getElementById("commet").innerHTML = response[i].body;
+            for (var u = 0; u < responseUsers.length; u++) {
+                if(responseUsers[u].id == response[i].userId){
+                    document.getElementById("userName").innerHTML = responseUsers[u].username;
+                    console.log(responsePhotos[u].url);
+                    document.getElementById("photo").src = responsePhotos[u].url;
+                    }
+                }
+            }
+        });
+    });
+});
+
+
+
+     
