e7b331c7f667d08b300ee98d723ec7026be57f72
Leiva Juan <Leiva Juan>
2018-11-26 23:33:14 -0300

    Avanzes sobre juego especifico

--- Practico-5/js/main.js
+++ Practico-5/js/main.js
@@ -6,7 +6,44 @@ let imgId = 'avatar';
 comments = '<div class= "comment"><img id=' + imgId + ' src="https://www.w3schools.com/howto/img_avatar.png" alt="avatar"><div><h5 id=' + nombreId + '>Robert</h5><span id=' + commentId + '>I just made a comment about this comment box which is purely made from CSS.</span></div></div>';
 /*$("#boxComment").append(comments);*/
 
-fetch('http://jsonplaceholder.typicode.com/users?_start=0&_limit=5')
+let start = 0;
+let limit = 2;
+
+document.getElementById("moreComments").addEventListener("click", function(){
+    start += 2;
+    limit += 1;
+    console.log(limit);
+    fetch('http://jsonplaceholder.typicode.com/users?_start=' + start + '&_limit= ' + limit +'')
+    .then(responseUsers => responseUsers.json())
+    .then(function(responseUsers){
+        fetch('http://jsonplaceholder.typicode.com/photos')
+        .then(responsePhotos => responsePhotos.json())
+        .then(function(responsePhotos){
+            fetch('http://jsonplaceholder.typicode.com/posts')  
+            .then(response => response.json())
+            .then(function(response){                 
+                for (var u = 0; u < responseUsers.length; u++) {
+                for (var i = 0; i < response.length; i++) {
+                    if(responseUsers[u].id == response[i].userId){
+                        commentId += cont;
+                        nombreId += cont;
+                        imgId += cont;
+                        comments = '<div class="comment"><img id=' + imgId + ' src="https://www.w3schools.com/howto/img_avatar.png" alt="avatar"><div><h5 id=' + nombreId + '>NombreUsuario</h5><span id=' + commentId + '>I just made a comment about this comment box which is purely made from CSS.</span></div></div>';
+                        $("#boxComment").append(comments);
+                        document.getElementById(commentId).innerHTML = response[i].body;
+                        document.getElementById(nombreId).innerHTML = responseUsers[u].username;
+                        document.getElementById(imgId).src = responsePhotos[u].url;
+                        cont++;
+                        i = response.length;
+                        }
+                    }
+                }
+            });
+        });
+    });
+});
+
+fetch('http://jsonplaceholder.typicode.com/users?_start=' + start + '&_limit= ' + limit +'')
   .then(responseUsers => responseUsers.json())
   .then(function(responseUsers){
     fetch('http://jsonplaceholder.typicode.com/photos')
